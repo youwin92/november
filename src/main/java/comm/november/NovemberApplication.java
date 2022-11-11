6923eeaa8f5d0abf56bbff95999158d4b3de93da
유승 <sksdu_1168@skshieldus.com>
2022-11-11 16:17:49 +0900

    first commit

---
+++ src/main/java/comm/november/NovemberApplication.java
@@ -0,0 +1,13 @@
+package comm.november;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NovemberApplication.class, args);
+    }
+
+}
---
+++ src/test/java/comm/november/NovemberApplicationTests.java
@@ -0,0 +1,13 @@
+package comm.november;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
